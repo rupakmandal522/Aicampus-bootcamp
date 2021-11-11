c7040ccf961c07dfbbd7ecc3741d0631e4e2af6a
rupakmandal522 <86112788+rupakmandal522@users.noreply.github.com>
2021-11-11 15:31:11 +0530

    javascript

---
+++ Html & css/pratice/Javascript/Introduction/JS/variable-naming/variable-naming.js
@@ -0,0 +1,15 @@
+// cannot contain spaces
+
+// var my name =10;X
+
+// Variable names must begin with a letter, an underscore (_) or a dollar sign ($)
+
+// var 1m1y1N1ame =10; We cannot start naming with number
+
+// var _myName =10; ?We can use underscore
+// console.log(_myName);
+// var $myName@ =10; Wre cannot use special character
+
+// var my_name_is_rupak;  Snake case
+
+// var myNameIsRpuak; camel case
